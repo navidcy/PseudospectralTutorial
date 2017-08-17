7588b5a2fe06b3b726f21d697f23bcde9cd97758
glwagner <wagner.greg@gmail.com>
2017-08-17 10:01:35 +0200

    Grooms in preparation for tutorial

--- threecodes/twodturb.jl
+++ threecodes/twodturb.jl
@@ -1,55 +1,66 @@
+#= twodturb.jl
+
+Solves the 2D vorticity equation using a Fourier pseudospectral method
+in a doubly-periodic box. Time-stepping is Forward Euler. Does not 
+dealias.
+=#
+
 using PyPlot
 
-nu = 2e-5
-Lx = 2.0*pi
-nx = 128
-dt = 1e-1
-nsteps = 10000
+
+# Parameters
+nx = 128            # Resolution 
+Lx = 2.0*pi         # Physical box size
+nu = 8e-5           # Laplacian viscosity
+dt = 1e-2           # Time step
+nsteps = 5000       # Number of time steps
 
 
 # Construct the grid
 ny, Ly = nx, Lx
-
-nk, nl = Int(nx/2+1), ny
 dx, dy = Lx/nx, Ly/ny
-  x, y = 0.0:dx:Lx-dx, 0.0:dy:Ly-dy 
+nk, nl = Int(nx/2+1), ny
 
-k = 2.0*pi/Lx * (0:nk)
-l = 2.0*pi/Ly * cat(1, 0:nl/2, -nl/2+1:-1)
+x = 0.0:dx:Lx-dx
+y = 0.0:dy:Ly-dy 
+k = 2.0*pi/Lx*(0:nk)
+l = 2.0*pi/Ly*cat(1, 0:nl/2, -nl/2+1:-1)
 
-X, Y = zeros(nx, ny), zeros(nx, ny)
-K, L = zeros(nk, nl), zeros(nk, nl)
+# 2D wavenumber grids with shapes (nk, nl)
+K = [ k[i] for i in 1:nk, j in 1:nl ]
+L = [ l[j] for i in 1:nk, j in 1:nl ]
 
-for i = 1:nx, j = 1:ny
-  X[i, j], Y[i, j] = x[i], y[j]
-end
-
-for i = 1:nk, j = 1:nl
-  K[i, j], L[i, j] = k[i], l[j]
-end
-
+# Need K^2 = k^2 + l^2 and 1/K^2.
 Ksq = K.^2.0 + L.^2.0
-invKsq = 1.0 ./ Ksq
-invKsq[1, 1] = 0.0
+invKsq = 1.0./Ksq
+invKsq[1, 1] = 0.0  # Will eliminate 0th mode during inversion
 
 
-# Initial condition
+# Random initial condition
 t = 0.0
 q = rand(nx, ny)
 qh = rfft(q)
 
+u = v = zeros(nx, ny)
+psih = rhs = zeros(nk, nl)
+
 
 # Step forward
-fig = figure()
-tic()
+fig = figure(); tic()
 for step = 1:nsteps
 
-  if (step-1) % 100 == 0
+  if step % 100 == 0 && step > 1
     clf(); imshow(q); pause(0.01)
-    @printf("step: %04d, t: %6.1f, ", step, t)
+
+    cfl = maximum(u)*dx/dt
+    @printf("step: %04d, t: %6.1f, cfl: %.2f, ", step, t, cfl)
     toc(); tic()
   end
 
+
+  # Calculate right hand side of vorticity equation.
+  # (Note that this algorithm is not as memory efficient as it 
+  # could be because it creates many temporary arrays.)
   psih = -invKsq .* qh
 
   q = irfft(qh, nx)
@@ -58,6 +69,20 @@ for step = 1:nsteps
 
   rhs = (-im).*K.*rfft(u.*q) .- im*L.*rfft(v.*q) .- nu*Ksq.*qh
 
+  # Step forward
   qh .+= dt.*rhs
   t += dt
 end
+
+
+# Final plot
+q = irfft(qh, nx)
+clf()
+pcolormesh(x, y, q)
+
+colorbar()
+xlabel(L"x")
+ylabel(L"y")
+title("\$q(x, y, t = $t)\$")
+
+show()
